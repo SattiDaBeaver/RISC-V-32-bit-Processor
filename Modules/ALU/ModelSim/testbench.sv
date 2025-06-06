e12b5f4ecd3ea474d8a71f66935834e8b21d6a8b
penguin <89865892+AayushChellani@users.noreply.github.com>
2025-06-05 21:11:51 -0400

    maybe working tb

--- Modules/ALU/ModelSim/testbench.sv
+++ Modules/ALU/ModelSim/testbench.sv
@@ -3,173 +3,176 @@
 
 module testbench ();
 
-	parameter CLOCK_PERIOD = 10;
+    parameter CLOCK_PERIOD = 10;
 
-	logic CLOCK_50;
+    logic CLOCK_50;
     logic [0:0] KEY;
     logic [6:0] HEX0;
 
-	// Comparison Unit Test
+    // Comparison Unit Test
     logic reset;
     logic [31:0] inputA;
     logic [31:0] inputB;
     logic [3:0]  ALUSelect;
     logic [31:0] dataOut;
     logic [31:0] dataOutHigh;
-    logic        compBit;
-    logic [31:0] compOutExpected;
+    logic [31:0] dataOutExpected;
+    logic [31:0] dataOutHighExpected;
 
-	initial begin
-        CLOCK_50 <= 1'b0;
-	end // initial
-	always @ (*)
-	begin : Clock_Generator
-		#((CLOCK_PERIOD) / 2) CLOCK_50 <= ~CLOCK_50;
-	end
-
+    // Parameters
     parameter dataWidth = 32;
     parameter selectWidth = 4;
 
-    enum logic [selectWidth-1:0] {
+    typedef enum logic [selectWidth-1:0] {
         ADD, SUB, MUL, AND, OR, 
         XOR, NOT, SLL, SRL
-    } ALUSelect;
-	
-	initial begin
-        #10
-        compSelect <= ADD; // ADD
-		inputA <= 32'h00000008; // 
-        inputB <= 32'h00000008; // 
-        compOutExpected <= ; // Expecting output to be 
-		#10
-        inputA <= 32'h00000008; // 
-        inputB <= 32'h00000007; // 
-        compOutExpected <= ; // Expecting output to be 
-		#20
+    } ALUOp;
 
-        compSelect <= SUB; // SUB
-		inputA <= 32'h00000003; // 
-        inputB <= 32'h00000007; // 
-        compOutExpected <= ; // Expecting output to be 
-		#10
-        inputA <= 32'h00000007; // 
-        inputB <= 32'h00000007; // 
-        compOutExpected <= ; // Expecting output to be 
-        #20
+    ALUOp compSelect;
 
-        compSelect <= LESS_THAN; // LESS THAN
-		inputA <= 32'h00000003; // 
-        inputB <= 32'h00000007; // 
-        compOutExpected <= 1'b1; // Expecting output to be 1
-		#10
-        inputA <= 32'h00000007; // 
-        inputB <= 32'h00000007; // 
-        compOutExpected <= 1'b0; // Expecting output to be 0
-        #10
-        inputA <= 32'h80000009; // 
-        inputB <= 32'h00000007; // 
-        compOutExpected <= 1'b1; // Expecting output to be 1
-        #20
+    initial begin
+        CLOCK_50 = 1'b0;
+    end
 
-        compSelect <= MUL; // MUL
-		inputA <= 32'h00000003; // 
-        inputB <= 32'h00000007; // 
-        compOutExpected <= ; // Expecting output to be 
-		#10
-        inputA <= 32'h00000007; // 
-        inputB <= 32'h00000007; // 
-        compOutExpected <= ; // Expecting output to be 
-        #10
-        inputA <= 32'h80000009; // 
-        inputB <= 32'h00000007; // 
-        compOutExpected <= ; // Expecting output to be 
-        #20
+    always begin
+        #(CLOCK_PERIOD / 2) CLOCK_50 = ~CLOCK_50;
+    end
 
-        compSelect <= AND; // AND
-		inputA <= 32'h00000003; // 
-        inputB <= 32'h00000007; // 
-        compOutExpected <= ; // Expecting output to be 
-		#10
-        inputA <= 32'h00000007; // 
-        inputB <= 32'h00000007; // 
-        compOutExpected <= ; // Expecting output to be 
+    initial begin
+        reset = 0;
+        ALUSelect = compSelect;
+
         #10
-        inputA <= 32'h80000009; // 
-        inputB <= 32'h00000007; // 
-        compOutExpected <= ; // Expecting output to be 
+        compSelect          = ADD;
+        ALUSelect           = compSelect;
+        inputA              = 32'h00000008;
+        inputB              = 32'h00000008;
+        dataOutExpected     = 32'h00000010;
+        dataOutHighExpected = 32'h0;
+
+        #10
+        inputA              = 32'h00000008;
+        inputB              = 32'h00000007;
+        dataOutExpected     = 32'h0000000F;
+        dataOutHighExpected = 32'h0;
 
         #20
-        compSelect <= OR; // OR
-		inputA <= 32'h00000003; // 
-        inputB <= 32'h00000007; // 
-        compOutExpected <= ; // Expecting output to be 
-		#10
-        inputA <= 32'h00000007; // 
-        inputB <= 32'h00000007; // 
-        compOutExpected <= ; // Expecting output to be 
+        compSelect          = SUB;
+        ALUSelect           = compSelect;
+        inputA              = 32'h00000003;
+        inputB              = 32'h00000007;
+        dataOutExpected     = 32'hFFFFFFFC;
+        dataOutHighExpected = 32'h0;
+
         #10
-        inputA <= 32'h80000009; // 
-        inputB <= 32'h00000007; // 
-        compOutExpected <= ; // Expecting output to be 
+        inputA              = 32'h00000007;
+        inputB              = 32'h00000007;
+        dataOutExpected     = 32'h00000000;
+        dataOutHighExpected = 32'h0;
 
         #20
-        compSelect <= XOR; // XOR
-		inputA <= 32'h80000003; // 
-        inputB <= 32'h00000007; // 
-        compOutExpected <= ; // Expecting output to be 
-		#10
-        inputA <= 32'h00000007; // 
-        inputB <= 32'h00000007; // 
-        compOutExpected <= ; // Expecting output to be 
+        compSelect          = MUL;
+        ALUSelect           = compSelect;
+        inputA              = 32'h00000003;
+        inputB              = 32'h00000007;
+        dataOutExpected     = 32'h00000015;
+        dataOutHighExpected = 32'h0;
+
         #10
-        inputA <= 32'h00000009; // 
-        inputB <= 32'h00000007; // 
-        compOutExpected <= ; // Expecting output to be 
+        inputA              = 32'h00000007;
+        inputB              = 32'h00000007;
+        dataOutExpected     = 32'h00000031;
+        dataOutHighExpected = 32'h0;
 
         #20
-        compSelect <= GREATER_THAN_UNSIGNED; // GREATER THAN UNSIGNED
-		inputA <= 32'h80000003; // 
-        inputB <= 32'h00000007; // 
-        compOutExpected <= 1'b1; // Expecting output to be 1
-		#10
-        inputA <= 32'h00000007; // 
-        inputB <= 32'h00000007; // 
-        compOutExpected <= 1'b0; // Expecting output to be 0
+        compSelect          = AND;
+        ALUSelect           = compSelect;
+        inputA              = 32'hF0F0F0F0;
+        inputB              = 32'h0F0F0F0F;
+        dataOutExpected     = 32'h00000000;
+        dataOutHighExpected = 32'h0;
+
         #10
-        inputA <= 32'h00000009; // 
-        inputB <= 32'h00000007; // 
-        compOutExpected <= 1'b1; // Expecting output to be 1
+        inputA              = 32'hFFFFFFFF;
+        inputB              = 32'h12345678;
+        dataOutExpected     = 32'h12345678;
+        dataOutHighExpected = 32'h0;
 
         #20
-        compSelect <= GREATER_THAN_OR_EQUAL; // GREATER THAN OR EQUAL
-		inputA <= 32'h80000003; // 
-        inputB <= 32'h00000007; // 
-        compOutExpected <= 1'b0; // Expecting output to be 0
-		#10
-        inputA <= 32'h00000007; // 
-        inputB <= 32'h00000007; // 
-        compOutExpected <= 1'b1; // Expecting output to be 1
+        compSelect          = OR;
+        ALUSelect           = compSelect;
+        inputA              = 32'hF0F0F0F0;
+        inputB              = 32'h0F0F0F0F;
+        dataOutExpected     = 32'hFFFFFFFF;
+        dataOutHighExpected = 32'h0;
+
         #10
-        inputA <= 32'h00000009; // 
-        inputB <= 32'h00000007; // 
-        compOutExpected <= 1'b1; // Expecting output to be 1
+        inputA              = 32'h00000000;
+        inputB              = 32'h12345678;
+        dataOutExpected     = 32'h12345678;
+        dataOutHighExpected = 32'h0;
 
         #20
-        compSelect <= GREATER_THAN_OR_EQUAL_UNSIGNED; // GREATER THAN OR EQUAL UNSIGNED
-		inputA <= 32'h80000003; // 
-        inputB <= 32'h00000007; // 
-        compOutExpected <= 1'b1; // Expecting output to be 0
-		#10
-        inputA <= 32'h00000007; // 
-        inputB <= 32'h00000007; // 
-        compOutExpected <= 1'b1; // Expecting output to be 1
+        compSelect          = XOR;
+        ALUSelect           = compSelect;
+        inputA              = 32'hFFFF0000;
+        inputB              = 32'h00FFFF00;
+        dataOutExpected     = 32'hFF00FF00;
+        dataOutHighExpected = 32'h0;
+
         #10
-        inputA <= 32'h00000009; // 
-        inputB <= 32'h00000007; // 
-        compOutExpected <= 1'b1; // Expecting output to be 1
-	end // initial
-	
-	ComparisonUnit #(
+        inputA              = 32'hAAAAAAAA;
+        inputB              = 32'h55555555;
+        dataOutExpected     = 32'hFFFFFFFF;
+        dataOutHighExpected = 32'h0;
+
+        #20
+        compSelect          = NOT;
+        ALUSelect           = compSelect;
+        inputA              = 32'h00000000;
+        inputB              = 32'h0;
+        dataOutExpected     = 32'hFFFFFFFF;
+        dataOutHighExpected = 32'h0;
+
+        #10
+        inputA              = 32'h12345678;
+        inputB              = 32'h0;
+        dataOutExpected     = 32'hEDCBA987;
+        dataOutHighExpected = 32'h0;
+
+        #20
+        compSelect          = SLL;
+        ALUSelect           = compSelect;
+        inputA              = 32'h00000001;
+        inputB              = 32'd32;
+        dataOutExpected     = 32'h00000000;
+        dataOutHighExpected = 32'h0;
+
+        #10
+        inputA              = 32'h00000001;
+        inputB              = 32'd1;
+        dataOutExpected     = 32'h00000002;
+        dataOutHighExpected = 32'h0;
+
+        #20
+        compSelect          = SRL;
+        ALUSelect           = compSelect;
+        inputA              = 32'h80000000;
+        inputB              = 32'd31;
+        dataOutExpected     = 32'h00000001;
+        dataOutHighExpected = 32'h0;
+
+        #10
+        inputA              = 32'h00000002;
+        inputB              = 32'd1;
+        dataOutExpected     = 32'h00000001;
+        dataOutHighExpected = 32'h0;
+
+        #20
+        $finish;
+    end
+
+    ALU #(
         .dataWidth(dataWidth),
         .selectWidth(selectWidth)
     ) comp_unit (
@@ -177,10 +180,9 @@ module testbench ();
         .reset(reset),
         .inputA(inputA), 
         .inputB(inputB), 
-        .comparisonSelect(compSelect),
-        
-        .dataOut(compOut)
+        .ALUSelect(ALUSelect),
+        .dataOut(dataOut),
+        .dataOutHigh(dataOutHigh)
     );
 
-    assign compBit = compOut[0];
 endmodule
